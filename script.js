92e991f0a3f30804b030b93f2858b3cab929c0d6
GodUsopp <an.brendancode@gmail.com>
2022-11-03 15:29:48 -0700

    Add constructor code

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.read = read;
+}
 
 function addBookToLibrary() {}
