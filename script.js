31598683497e83d127331676dbbd497396f59d68
GodUsopp <an.brendancode@gmail.com>
2022-11-10 17:51:29 -0800

    Fix event listeners
    fixed previous event listeners and added functions to the table
    buttons. Also added function to clear inputs.

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const add = document.querySelector(".add-button1");
 const close = document.querySelector(".close-button");
 const overlay = document.querySelector(".overlay");
 const submit = document.querySelector(".submit");
+const readButton = document.querySelector(".read-button");
+const deleteButton = document.querySelector(".delete-button");
 
 function Book(title, author, read) {
   this.title = title;
@@ -27,7 +29,12 @@ function closeTheForm() {
   overlay.style.display = "none";
 }
 
+function resetForm() {
+  document.querySelector("form").reset();
+}
+
 add.addEventListener("click", () => {
+  resetForm();
   openTheForm();
 });
 
@@ -63,6 +70,13 @@ function addLibrary() {
         btn.classList.add("table-button", "read-button");
         tableData.appendChild(btn);
         tableRow.appendChild(tableData);
+        btn.addEventListener("click", () => {
+          if (btn.textContent == "Read") {
+            btn.textContent = "Unread";
+          } else {
+            btn.textContent = "Read";
+          }
+        });
       } else if (object.hasOwnProperty(key)) {
         const tableData = document.createElement("td");
         tableData.textContent = `${object[key]}`;
@@ -75,6 +89,11 @@ function addLibrary() {
   btn.classList.add("table-button", "delete-button");
   tableData.appendChild(btn);
   tableRow.appendChild(tableData);
+  btn.addEventListener("click", () => {
+    btn.addEventListener("click", (event) => {
+      event.target.parentNode.parentNode.remove();
+    });
+  });
 }
 
 submit.addEventListener("click", () => {
@@ -82,8 +101,20 @@ submit.addEventListener("click", () => {
   let bookAuthor = document.querySelector(".book-author").value;
   let readOrUnread = document.querySelector("input[name=read]:checked").value;
 
-  let newBook = addBookToLibrary(bookTitle, bookAuthor, readOrUnread);
+  addBookToLibrary(bookTitle, bookAuthor, readOrUnread);
   closeTheForm();
   addLibrary();
   myLibrary = [];
+});
+
+readButton.addEventListener("click", () => {
+  if (readButton.textContent == "Read") {
+    readButton.textContent = "Unread";
+  } else {
+    readButton.textContent = "Read";
+  }
+});
+
+deleteButton.addEventListener("click", (event) => {
+  event.target.parentNode.parentNode.remove();
 });
